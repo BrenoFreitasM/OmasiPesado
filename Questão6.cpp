454bddb53389b4d694ebd9631db3c7284beb1bdc
Breno Freitas <brenofreitasmendes@gmail.com>
2021-08-28 19:04:03 -0300

    Afinal o que é o primeiro commit

---
+++ Questão6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	setlocale(LC_ALL,"Portuguese");
+	int I=1,P;
+//	int PaMA = 9999;
+//	int PbMEN = 0; 
+
+int InMgor = 0;
+int Mgor = 0;
+
+	while(I>0){
+		I--;
+	cout<<"	número de indetificação: " ;
+	cin>> I;
+	cout<<"Peso do boi: ";
+	cin>>  P;
+			if(Mgor<P){
+			Mgor-Mgor;
+			Mgor=+P;
+			InMgor-InMgor;
+			InMgor =+ I;}
+	
+	}
+	cout<<"\n"<<"A indentificalção do boi mais pesado é:..."<<InMgor<<"\n";
+	    	cout<<"O peso do boi mais pesado é:.............."<<Mgor;
+}
